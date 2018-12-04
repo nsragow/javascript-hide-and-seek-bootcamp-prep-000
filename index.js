f5fd358b61e0f5a86cb29995a6230713ce16f2fb
Noah Sragow <nsragow@gmail.com>
2018-12-04 20:28:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector)
+}
 
+function nestedTarget()
+{
+  return document.querySelector("#nested .target")
+}
+
+function deepestChild()
+{
+  var next = document.querySelector("div#grand-node")
+  while(true)
+  {
+    var temp = next.querySelector("*")
+    if(temp === null)
+    {
+      return next;
+    }
+    next = temp
+  }
+}
+
+
+
+
+
+function increaseRankBy(n)
+{
+  var toChange = document.querySelectorAll(".ranked-list *")
+  for(var i = 0; i < toChange.length; i++)
+  {
+    toChange[i].innerHTML = parseInt(toChange[i].innerHTML)+n
+  }
+}
